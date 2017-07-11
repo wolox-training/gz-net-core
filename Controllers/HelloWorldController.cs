183e820c13db01cf77212ff20b7ac9d913f7198e
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2017-07-11 17:29:52 -0300

    Add HelloWorldController

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,14 +5,12 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /HelloWorld/
         public string Index()
         {
             return "This is my default action...";
         }
 
-        // GET: /HelloWorld/Welcome/ 
-        public string Welcome(string name, int ID = 1)
+       public string Welcome(string name, int ID = 1)
         {
              return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
