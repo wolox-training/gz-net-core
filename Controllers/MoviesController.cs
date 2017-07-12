b564454a7dd1d737d97238e54c409e0a9f64889a
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2017-07-12 16:39:14 -0300

    Add Movie Model

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public string Index()
+        {
+            return "";
+        }
+    }
+}
---
+++ Models/DataBaseContext.cs
@@ -0,0 +1,22 @@
+// Models/DataBaseContext.cs
+
+#region Using
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+#endregion
+
+namespace MvcMovie.Models
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) {}
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
