3ded361f6750c7e3717fc823ad4bc62a8c572627
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2017-07-12 13:50:37 -0300

    Fixes for review

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,25 +8,11 @@ namespace MvcMovie.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public string Index()
         {
-            return View();
+            return "";
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+        
         public IActionResult Error()
         {
             return View();
