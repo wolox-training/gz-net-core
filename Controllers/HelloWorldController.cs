cf42f08d9923703d09b0dab6adbb27cf7b817591
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2017-07-12 14:19:52 -0300

    Fixes for review

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,17 +5,13 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // 
         // GET: /HelloWorld/
-
         public string Index()
         {
             return "This is my default action...";
         }
 
-        // 
         // GET: /HelloWorld/Welcome/ 
-
         public string Welcome(string name, int ID = 1)
         {
              return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
