688cecd015ff49327cb36ceba7f7e16197889866
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2017-07-14 14:25:57 -0300

    Seccion principal

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,9 +8,9 @@ namespace MvcMovie.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "";
+            return View();
         }
         
         public IActionResult Error()
