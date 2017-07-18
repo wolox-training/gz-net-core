8d86b9da183d027b506d88191d183039f0a4acb2
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2017-07-18 16:01:01 -0300

    Add Movies/Index

---
+++ Models/Movie.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
