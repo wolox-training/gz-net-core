22c1c0191e90670e5db6669aa904824650e34e66
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2017-07-19 15:07:56 -0300

    Added details and delete

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -91,6 +91,33 @@ namespace MvcMovie.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult Details(int? id)
+        {
+            if (id == null) 
+            {
+                return NotFound();
+            }
+
+            Movie movie = movieRepository.GetById(id);
+
+            if (movie == null) {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null) 
+            {
+                return NotFound();
+            }
+            movieRepository.Delete(id);
+
+            return RedirectToAction("Index");
+        }
+
         public MovieRepository movieRepository
         {            
             get { return _movieRepository; }        
